99ee95a2b5649dfd348bcc2fbff00e3f70f51b6c
Janis Malcans <jmalcans@evolution.com>
2026-03-20 23:30:38 +0200

    Update theme config

--- init.lua
+++ init.lua
@@ -335,7 +335,17 @@ require("lazy").setup({
             "neovim/nvim-lspconfig",
 
             -- Useful status updates for LSP.
-            { "j-hui/fidget.nvim", opts = {} },
+            {
+                "j-hui/fidget.nvim",
+                opts = {
+                    notification = {
+                        window = {
+                            winblend = 0,
+                            normal_hl = "Comment",
+                        },
+                    },
+                },
+            },
 
             -- Allows extra capabilities provided by blink.cmp
             "saghen/blink.cmp",
@@ -646,31 +656,6 @@ require("lazy").setup({
             -- Shows a signature help window while you type arguments for a function
             signature = { enabled = true },
         },
-    },
-
-    { -- You can easily change to a different colorscheme.
-        -- Change the name of the colorscheme plugin below, and then
-        -- change the command in the config to whatever the name of that colorscheme is.
-        --
-        -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-        "folke/tokyonight.nvim",
-        priority = 1000, -- Make sure to load this before all the other start plugins.
-        config = function()
-            ---@diagnostic disable-next-line: missing-fields
-            require("tokyonight").setup({
-                -- transparent = true,
-                styles = {
-                    comments = { italic = false }, -- Disable italics in comments
-                    -- sidebars = 'transparent',
-                    -- floats = 'transparent',
-                },
-            })
-
-            -- Load the colorscheme here.
-            -- Like many other themes, this one has different styles, and you could load
-            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme("tokyonight-moon")
-        end,
     },
 
     -- Highlight todo, notes, etc in comments

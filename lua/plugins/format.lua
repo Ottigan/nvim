3780b8118c8c49b942b1183d1d1f33f2bb19956c
Janis Malcans <jmalcans@evolution.com>
2026-04-17 01:18:30 +0300

    Update format.lua according to conform docs

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,9 +1,3 @@
-local FORMAT_OPTIONS = {
-    timeout_ms = 3000,
-    async = true,
-    lsp_format = "fallback",
-}
-
 return {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
@@ -12,34 +6,15 @@ return {
         {
             "<leader>f",
             function()
-                require("conform").format(FORMAT_OPTIONS)
+                require("conform").format({ async = true, })
             end,
             mode = "",
             desc = "[F]ormat buffer",
         },
     },
+    ---@module "conform"
+    ---@type conform.setupOpts
     opts = {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-            -- Disable "format_on_save lsp_fallback" for languages that don't
-            -- have a well standardized coding style. You can add additional
-            -- languages here or re-enable it for the disabled ones.
-            local disable_filetypes = { c = true, cpp = true }
-
-            -- Disable for TypeScript/JavaScript - handled manually in ts_ls autocmd
-            local ts_filetypes = {
-                typescript = true,
-                javascript = true,
-                typescriptreact = true,
-                javascriptreact = true,
-            }
-
-            if disable_filetypes[vim.bo[bufnr].filetype] or ts_filetypes[vim.bo[bufnr].filetype] then
-                return nil
-            else
-                return FORMAT_OPTIONS
-            end
-        end,
         formatters_by_ft = {
             lua = { "stylua" },
             javascript = { "eslint_d" },
@@ -47,9 +22,24 @@ return {
             javascriptreact = { "eslint_d" },
             typescriptreact = { "eslint_d" },
             scss = { "stylelint" },
-
-            -- Conform can also run multiple formatters sequentially
-            -- python = { "isort", "black" },
         },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        format_on_save = function(bufnr)
+            -- Disable for TypeScript/JavaScript - handled manually in ts_ls autocmd
+            local disable_filetypes = {
+                "typescript",
+                "javascript",
+                "typescriptreact",
+                "javascriptreact"
+            }
+
+            if vim.tbl_contains(disable_filetypes, vim.bo[bufnr].filetype) then
+                return
+            end
+
+            return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
     },
 }
